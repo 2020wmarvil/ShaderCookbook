817fef280b9b967da9a68e3405bcf7d340dd4c7b
Wyatt Marvil <2020wmarvil@gmail.com>
2021-12-03 09:33:15 -0500

    Post process gifs

--- ShaderExperimentation/Assets/Shaders/ImageEffects/ScreenTransitions/ScreenTransitionImageEffectController.cs
+++ ShaderExperimentation/Assets/Shaders/ImageEffects/ScreenTransitions/ScreenTransitionImageEffectController.cs
@@ -5,10 +5,18 @@ public class ScreenTransitionImageEffectController : MonoBehaviour {
 	[SerializeField] float duration;
 	
 	float cutoff = 0f;
+	bool running = false;
 
 	void Update() {
-		cutoff += Time.deltaTime / duration;
-		if (Input.GetKeyDown(KeyCode.Space)) cutoff = 0f;
+		if (running) {
+			cutoff += Time.deltaTime / duration;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			cutoff = 0f;
+			running = true;
+		}
+
 		effect.SetCutoff(cutoff);
 	}
 }
